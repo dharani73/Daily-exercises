1ae6dd9a5458aa0dcc14726b25c9e239ca628ce3
SaiDharani Miriyala <miriyala.dharani.@gmail.com>
2022-08-18 14:57:45 +0530

    commit

---
+++ pptexample/Demo/demo.js
@@ -0,0 +1,16 @@
+// function isEven(num) {
+//   let isEven;
+//   if (num % 2 == 0) {
+//     isEven = true;
+//   } else {
+//     isEven = false;
+//   }
+//   return isEven;
+// }
+
+// console.log(isEven(5));
+
+for (i = 0; i < 5; i++) {
+  console.log(i);
+}
+console.log('end');
